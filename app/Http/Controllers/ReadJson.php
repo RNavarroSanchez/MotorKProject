f94da965f38a90d941ff52a3bda17e120c8f29dd
Roberto Navarro Sánchez <robertonavarrosanchez@gmail.com>
2024-01-28 20:01:18 +0100

    mejorando codigo

--- app/Http/Controllers/ReadJson.php
+++ app/Http/Controllers/ReadJson.php
@@ -5,30 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Vehicle;
+use Illuminate\Support\Facades\Storage;
+
 
 class ReadJson extends Controller
 {
     public function showDataFromJson()
     {
-       // Código para leer el JSON y decodificar los datos
-       $contentJson = file_get_contents(storage_path('app/json/vehicles.json'));
-       $vehicles = json_decode($contentJson, true);
+        $filePath = 'json/vehicles.json'; 
+        $contentJson = Storage::get($filePath);
+        $vehiclesData = json_decode($contentJson, true);
 
-       // Verificar si hay datos
-       if ($vehicles)
-        {       
-            $vehiclesjson = [];
-            foreach ($vehicles as $vehiclesData) {
-                
-                $vehiclesjson[] = new Vehicle($vehiclesData);
-            }
-            
-            return view('welcome', ['vehicles' => $vehiclesjson]);
+        if ($vehiclesData) {
+            $vehicles = collect($vehiclesData)->map(function ($vehicleData) {
+                return new Vehicle($vehicleData);
+            });
+
+            return view('welcome', ['vehicles' => $vehicles]);
+        } else {
+            // Puedes lanzar una excepción o devolver una respuesta HTTP
+            return response()->json(['message' => 'No se encontraron datos en el JSON.'], 404);
         }
-       else 
-       {
-           echo "No se encontraron datos en el JSON.";
-       }  
-   }
-
+    }
 }
